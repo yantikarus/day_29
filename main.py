9dc6e401c82abc0c91d0ec228d5afa933957cc26
Yanti <wsutr@hotmail.com>
2023-03-27 21:49:13 -0700

    add UI layout

---
+++ main.py
@@ -0,0 +1,47 @@
+from tkinter import *
+
+# ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+# ---------------------------- SAVE PASSWORD ------------------------------- #
+
+# ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title("Password Manager")
+window.config(padx=50, pady=50)
+
+
+canvas = Canvas(width=200, height=200)
+lock_img = PhotoImage(file="logo.png")
+canvas.create_image(100, 100, image=lock_img)
+canvas.grid(row=0, column=1)
+
+#Label
+website = Label(text="Website:")
+website.grid(row=1, column=0)
+
+email_username = Label(text="Email/Username:")
+email_username.grid(row=2, column=0)
+
+password = Label(text="Password:")
+password.grid(row=3, column=0)
+
+
+#Entry
+
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+#Button
+generate_pass_button = Button(text="Generate Password", padx=0, pady=0, highlightthickness=0)
+generate_pass_button.grid(row=3, column=2)
+
+add_button = Button(text="Add", width=35, padx=0, pady=0, highlightthickness=0)
+add_button.grid(row=4, column=1, columnspan=2)
+
+window.mainloop()
